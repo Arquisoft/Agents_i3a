5d0079e7ea5d765fc459a3520d71de1c67ed1491
Víctor Suárez Fernández <vicsufer@gmail.com>
2018-02-03 11:02:03 +0100

    [HOTFIX] CSV format

--- src/main/java/util/CSVReader.java
+++ src/main/java/util/CSVReader.java
@@ -15,7 +15,7 @@ public class CSVReader {
     public static String getValueForKind(int kindCode) throws IOException
     {
 	Reader in = new FileReader(MASTER_FILE_PATH);
-	Iterable<CSVRecord> records = CSVFormat.newFormat(';').parse(in);
+	Iterable<CSVRecord> records = CSVFormat.newFormat(',').parse(in);
 	for (CSVRecord record : records) {
 	    int code = Integer.parseInt( record.get(0) );
 	    if(code == kindCode)
