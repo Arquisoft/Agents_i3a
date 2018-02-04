efd8c42b020a9223fb345dc97472ca1ded370e1d
Guillermo Facundo Colunga <colunga91@gmail.com>
2018-02-04 13:07:14 +0100

    Improved AgentTest
    - Removed unnecessary calendar instances.
    - Added in-code comments.
    - Added tests for null values and empty strings.
    - Added test for the `compareTo()`.
    - Improved existing tests.

--- src/test/java/domainTests/AgentTest.java
+++ src/test/java/domainTests/AgentTest.java
@@ -14,7 +14,6 @@ package domainTests;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.util.Calendar;
 
 import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.junit.Before;
@@ -25,15 +24,19 @@ import Foundation.URL;
 import domain.Agent;
 
 /**
- * Created by Damian on 15/02/2017.
  * 
- * Adapter by Víctor on 02/02/2018
+ * Test for the domain instance of agent.
+ * 
+ * Adapted by Víctor on 02/02/2018.
+ * Modified by Guillermo on 201802041300.
+ *
+ * @author Damian
+ * @version 201802041300
+ * @since 15/02/2017
  */
 public class AgentTest {
 
-    private Agent nico;
-    private Agent jorge;
-    private Agent damian;
+    private Agent nico, jorge, damian;
 
     /**
      * Also a resources/master.csv file is needed with the following rows: 1 ,
@@ -43,127 +46,194 @@ public class AgentTest {
      */
     @Before
     public void setUp() throws IOException {
-	Calendar cal = Calendar.getInstance();
-	cal.set(1996, Calendar.JUNE, 12);
 	nico = new Agent("Nicolás", "nico@nicomail.com", "nico123");
 	jorge = new Agent("Jorge", "45N30E", "jorge@jorgemail.com", "jorge123", "111111111A", 2);
-	cal.set(1997, Calendar.AUGUST, 1);
 	damian = new Agent("Damian", "45S30W", "damian@damianmail.com", "damian123", "222222222B", 3);
     }
 
     @Test
-    public void firstNameTest() {
+    public void nameTest() {
+
+	// Getting the previously set names.
 	assertEquals("Nicolás", nico.getName());
 	assertEquals("Jorge", jorge.getName());
 	assertEquals("Damian", damian.getName());
 
+	// Changing and testing the name of Nico to Antonio.
 	nico.setName("Antonio");
 	assertEquals("Antonio", nico.getName());
 
+	// Same for Jorge.
 	jorge.setName("Pepe");
 	assertEquals("Pepe", jorge.getName());
 
+	// Same for Roberto.
 	damian.setName("Roberto");
 	assertEquals("Roberto", damian.getName());
+
+	// Test for the empty string.
+	nico.setName("");
+	assertEquals("", nico.getName());
+
+	// Test for null values. Must return at least the empty string.
+	nico.setName(null);
+	assertEquals("", nico.getName());
     }
 
     @Test
     public void locationTest() {
 
+	// Setting to nico the same location as jorge.
 	nico.setLocation(jorge.getLocation());
-	assertEquals("45N30E", nico.getLocation());
+	assertEquals(jorge.getLocation(), nico.getLocation());
 
+	// Now jorge has the same location as damian.
 	jorge.setLocation(damian.getLocation());
-	assertEquals("45S30W", jorge.getLocation());
+	assertEquals(damian.getLocation(), jorge.getLocation());
 
+	// Hard coding a location.
 	damian.setLocation("30N48E");
 	assertEquals("30N48E", damian.getLocation());
+
+	// The empty string.
+	damian.setLocation("");
+	assertEquals("", damian.getLocation());
+
+	// Test for null values. Must return at least the empty string.
+	damian.setLocation(null);
+	assertEquals("", damian.getLocation());
     }
 
     @Test
     public void emailTest() {
 
+	// Setting to nico the email of damian.
 	nico.setEmail(damian.getEmail());
-	assertEquals("damian@damianmail.com", nico.getEmail());
+	assertEquals(damian.getEmail(), nico.getEmail());
 
+	// Hard coding an email for jorge.
 	jorge.setEmail("pepe@pepemail.com");
 	assertEquals("pepe@pepemail.com", jorge.getEmail());
 
+	// Setting to damian the email of jorge that was previously hard coded.
 	damian.setEmail(jorge.getEmail());
 	assertEquals("pepe@pepemail.com", damian.getEmail());
+
+	// Empty string check.
+	nico.setEmail("");
+	assertEquals("", nico.getEmail());
+
+	// Test for null values. Must return at least the empty string.
+	damian.setEmail(null);
+	assertEquals("", damian.getEmail());
     }
 
     @Test
     public void passwordTest() {
 
+	// Creating the encrypt instance.
 	StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
 
+	// Setting the password of nico to 1234. And checking the encrypt process works.
 	nico.setPassword("1234");
-
 	assertTrue(encryptor.checkPassword("1234", nico.getPassword()));
 
-	jorge.setPassword("abcd");
-	assertTrue(encryptor.checkPassword("abcd", jorge.getPassword()));
+	// Checking that the password is not being stored as plain text.
+	assertNotEquals("1234", nico.getPassword());
 
-	damian.setPassword("yay");
-	assertTrue(encryptor.checkPassword("yay", damian.getPassword()));
+	// Checking the empty string as a password.
+	nico.setPassword("");
+	assertTrue(encryptor.checkPassword("", nico.getPassword()));
+
+	// Checking that the password is not being stored as plain text.
+	assertNotEquals("", nico.getPassword());
+
+	// Check for the null string as a password.
+	nico.setPassword(null);
+	assertTrue(encryptor.checkPassword("", nico.getPassword()));
+
+	// Checking that the password is not being stored as plain text.
+	assertNotEquals("", nico.getPassword());
     }
 
     @Test
-    public void nifTest() {
+    public void identifierTest() {
 
-	assertEquals(null, nico.getId());
+	// Nico has its id set to null, jorge to 111111111A and damian to 222222222B.
+	// So testing that a null id will return at least the empty string.
+	assertEquals("", nico.getId());
 
+	// Checking that the correct id is returned.
 	assertEquals("111111111A", jorge.getId());
 
+	// Same for damian.
 	assertEquals("222222222B", damian.getId());
     }
 
     @Test
     public void kindTest() throws IOException {
+	// The actual CSVFile contains 1,Person;2,Entity;3,Sensor.
+	// Setting nico to Person (1); And checking the resolution of the name work
+	// properly.
 	nico.setKindCode(1);
 	assertEquals("Person", nico.getKind());
+
+	// Nico was previously set to (2) Entity. Checking that both, the code and the
+	// resolution work.
 	assertEquals("Entity", jorge.getKind());
 	assertEquals(2, jorge.getKindCode());
 
+	// Same for the Sensor damian.
 	assertEquals("Sensor", damian.getKind());
 	assertEquals(3, damian.getKindCode());
 
-	nico.setKindCode(1);
-	assertEquals("Person", nico.getKind());
-	assertEquals(1, nico.getKindCode());
-
+	// Changing the value of jorge to a Person and checking now the resolution of
+	// the kind of agent still works.
 	jorge.setKindCode(1);
 	assertEquals("Person", jorge.getKind());
 	assertEquals(1, jorge.getKindCode());
 
-	CSVFile previous = CSVFile.of(new URL("src/main/resources/master.csv"), ",");
+	// Creating a new CSV file with diffetent attributes that the old one. Checking
+	// that if the file changes at execution times the resolution of names still
+	// works.
+	CSVFile originalFile = CSVFile.of(new URL("src/main/resources/master.csv"), ",");
 	CSVFile testFile = new CSVFile(new URL("src/main/resources/master.csv"));
 	testFile.setSeparator(",");
-	String[] person = { "Person" },
-		entity = { "Entity" },
-		sensor = { "Sensor" },
-		iphone = { "iPhone" };
+	String[] person = { "Person" }, entity = { "Entity" }, sensor = { "Sensor" }, iphone = { "iPhone" };
 	testFile.getContent().put("1", person);
 	testFile.getContent().put("2", entity);
 	testFile.getContent().put("3", sensor);
 	testFile.getContent().put("4", iphone);
 	testFile.save();
 
+	// Now we set nico to 4, a value that was not in the previous master.csv file.
 	jorge.setKindCode(4);
 	assertEquals("iPhone", jorge.getKind());
+	
+	// We swap the entity and the person values in the csv file
 	testFile.getContent().replace("1", entity);
 	testFile.getContent().replace("2", person);
 	testFile.save();
+	
+	// Checking the resolution of the agent kind still works.
 	assertEquals("Entity", nico.getKind());
 
-	previous.save();
+	// We revert changes and save the original file.
+	originalFile.save();
+	
+	// Checking if we assign a kindCode that is not in the file
+	nico.setKindCode(-1);
+	assertEquals(Agent.KIND_NOT_FOUND, nico.getKind());
 
     }
 
     @Test
     public void toStringTest() {
+	
+	// Ensuring the kind code for nico test.
 	nico.setKindCode(1);
+	
+	// Checking the format of the toString.
 	assertEquals("{" + "name='" + nico.getName() + "'," + "location='" + nico.getLocation() + "'," + "email='"
 		+ nico.getEmail() + "'," + "id='" + nico.getId() + "'," + "kind='" + nico.getKind() + "',"
 		+ "kindCode='" + nico.getKindCode() + "'" + "}", nico.toString());
@@ -171,21 +241,52 @@ public class AgentTest {
 
     @Test
     public void equalsTest() {
+	
+	// Creating a copy of nico.
 	Agent copyOfNico = new Agent(nico.getName(), nico.getLocation(), nico.getEmail(), nico.getPassword(), "111",
 		nico.getKindCode());
+	
+	// Another copy of nico.
 	Agent anotherCopyOfNico = new Agent(nico.getName(), nico.getLocation(), nico.getEmail(), nico.getPassword(),
 		"111", nico.getKindCode());
 
+	// Both copies must be equal.
 	assertEquals(true, anotherCopyOfNico.equals(copyOfNico));
+	
+	// Creating a copy of nico that has a different id.
+	Agent nonEqualCopyOfNico = new Agent(nico.getName(), nico.getLocation(), nico.getEmail(), nico.getPassword(),
+		"112", nico.getKindCode());
+	
+	// Checking now the copies are not equal.
+	assertNotEquals(true, copyOfNico.equals(nonEqualCopyOfNico));
     }
 
     @Test
     public void kindCodeTest() {
-
+	
+	// Setting the kind code of jorge to be 2.
 	jorge.setKindCode(2);
 	assertEquals(2, jorge.getKindCode());
+	
+	// Setting the kind code of damian to be 3.
 	damian.setKindCode(3);
 	assertEquals(3, damian.getKindCode());
+	
+	// Checking negative integers.
+	nico.setKindCode(-1);
+	assertEquals(-1, nico.getKindCode());
+    }
+
+    @Test
+    public void compareToTest() {
+	// Creating to Agents where jorge has a bigger id than nico.
+	nico = new Agent("nico", "10W10E", "nico@nico.com", "12345", "11111111A", 1);
+	jorge = new Agent("jorge", "10W10E", "jorge@jorge.com", "12345", "11111111B", 1);
+	
+	// Checking all possibilities of the equals.
+	assertEquals(-1, nico.compareTo(jorge));
+	assertEquals(0, nico.compareTo(nico));
+	assertEquals(1, jorge.compareTo(nico));
     }
 
 }
