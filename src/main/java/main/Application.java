32e3c298862f6c10ea28d72243ecd25a16dc989a
Guillermo Facundo Colunga <colunga91@gmail.com>
2018-03-28 17:55:36 +0200

    Revision done
    - Add tag for the service discovery system
    
    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -13,10 +13,12 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableMongoRepositories("dbmanagement")
 @ComponentScan({ "dbmanagement", "controllers", "domain", "services" })
